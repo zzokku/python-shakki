ea755437abecf5020cef3da68a660d6ada2ecac6
zzokku <93818329+zzokku@users.noreply.github.com>
2022-12-17 21:25:11 +0200

    muutettiin laudan rakennetta ja siihen littyvää koodia

--- shakki/shakki.py
+++ shakki/shakki.py
@@ -2,19 +2,19 @@ class Shakki:
 
     """ 
     Merkkijonon ensimmäinen kirjain kertoo shakkinappulan värin 
-    esim. m tarkoittaa mustaa. Toinen / kolmas kirjain taas kertoo shakkinappulan nimen esim. 
-    k = kuningas, d = daami = kuningatar = , l = lähetti ja r = ratsu.
+    esim. m tarkoittaa mustaa. Toinen kirjain taas kertoo shakkinappulan nimen esim. 
+    K = kuningas, D = daami = kuningatar, L = lähetti ja R = ratsu.
     """
 
     lauta = [
-        ["mt", "mr", "ml", "md", "mk", "ml", "mr", "mt"],
-        ["ms", "ms", "ms", "ms", "ms", "ms", "ms", "ms"],
-        ["", "", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", "", ""],
-        ["vs", "vs", "vs", "vs", "vs", "vs", "vs", "vs"],
-        ["vt", "vr", "vl", "vk", "vkg", "vl", "vr", "vt"]
+        "mT", "mR", "mL", "mK", "mD", "mL", "mR", "mT",
+        "mS", "mS", "mS", "mS", "ms", "mS", "mS", "mS",
+        "", "", "", "", "", "", "", "",
+        "", "", "", "", "", "", "", "",
+        "", "", "", "", "", "", "", "",
+        "", "", "", "", "", "", "", "",
+        "vS", "vS", "vS", "vS", "vS", "vS", "vS", "vS",
+        "vT", "vR", "vL", "vD", "vK", "vL", "vT", "vT"
     ]
 
     def __init__(self) -> None:
@@ -25,51 +25,59 @@ class Shakki:
         return self.lauta
 
     # Tulostaa pelihetkeä vastaavan laudan
-    def tulostaLauta(self) -> str:
-        return str(self.lauta)
+    def tulostaLauta(self) -> None:
+        for i in range(0, len(self.lauta)):
+            if self.lauta[i] != "":
+                print(self.lauta[i], end="  ")
+            else:
+                print("·", end="   ")
+            if (i+1) % 8 == 0:
+                print("\n")
 
-    # str metodi tulostaa saman kuin metodi tulostaLauta
-    def __str__(self) -> str:
-        return self.tulostaLauta
 
-    def ulosLaudalta(self, koordinaatti1: tuple, koordiaatti2: tuple) -> bool:
-        for alkio in koordinaatti1+koordiaatti2:
-            if not -1 < alkio < 8:
-                return True
-        return False
+    
+    # Tarkistaa onko nappula ulos laudalta
+    def ulosLaudalta(self, koordinaatti: int) -> bool:
+        return -1 < koordinaatti < 64
+
+    # Tarkistaa onko lauta vielä siirron jälkeen pelisääntöjen mukainen
+    def laillinenLauta() -> None:
+        pass
+
 
     # Siirtää shakkinappulan laudalla
-    def siirto(self, koordinaatti1: tuple, koordinaatti2: tuple) -> None:
-        self.board[koordinaatti2[0]][koordinaatti2[1]] = self.board[koordinaatti1[0]][koordinaatti1[1]]
-        self.board[koordinaatti1[0]][koordinaatti1[1]] = ""
+    def siirto(self, koordinaatti1: int, koordinaatti2: int) -> None:
+        self.lauta[koordinaatti2] = self.lauta[koordinaatti1]
+        self.lauta[koordinaatti1] = ""
         
     
     # Palauttaa listan kaikista värin laillisista siirroista
-    def __laillisetSiirrot(self, vari: str) -> list:
+    def __laillisetSiirrot(self, vari: int) -> list:
         pass
 
     # Ratsun lailliset siirrot värin ja koordinaatin perusteella
-    def __rLailliset(self, vari: str, koordinaatti: tuple) -> list:
+    def __rLailliset(self, vari: int, koordinaatti: int) -> list:
         pass
 
     # Lähetin lailliset siirrot värin ja koordinaatin perusteella
-    def __lLailliset(self, vari: str, koordinaatti: tuple) -> list:
+    def __lLailliset(self, vari: int, koordinaatti: int) -> list:
         pass
 
     # Tornin lailliset siirrot värin ja koordinaatin perusteella
-    def __tLailliset(self, vari: str, koordinaatti: tuple) -> list:
+    def __tLailliset(self, vari: int, koordinaatti: int) -> list:
         pass
 
     # Soturin lailliset siirrot värin ja koordinaatin perusteella
-    def __sLailliset(self, vari: str, koordinaatti: tuple) -> list:
+    def __sLailliset(self, vari: int, koordinaatti: int) -> list:
         pass
 
+
     # Kuninkaan lailliset siirrot värin ja koordinaatin perusteella
-    def kLailliset(self, vari: str, koordinaatti: tuple) -> list:
+    def kLailliset(self, vari: int, koordinaatti: int) -> list:
         pass
 
     # Daamin lailliset siirrot värin ja koordinaatin perusteella
-    def dLailliset(self, vari: str, koordinaatti: tuple) -> list:
+    def dLailliset(self, vari: int, koordinaatti: int) -> list:
         pass
 
         
