39864f489a3f85d34e091f95b6bf8b5d773f2a2a
zzokku <93818329+zzokku@users.noreply.github.com>
2022-12-19 22:43:58 +0200

    reunukset laudalle, josta havaitaan että ruutu ei kuulu laudalle

--- shakki/main.py
+++ shakki/main.py
@@ -2,7 +2,9 @@ from shakki import Shakki
 
 
 shakki = Shakki()
+
+shakki.siirto(20, 30)
+shakki.siirto(21,31)
+print(shakki.tLailliset("m"))
 shakki.pelilauta()
-print(shakki.nappulaRyhma("m", "S"))
-print(shakki.sLailliset("m"))
 
