650acce1db00184893d0119b16a554166a91b471
zzokku <93818329+zzokku@users.noreply.github.com>
2022-12-22 16:38:23 +0200

    alustava pelifunktio ja koordinaatisto sanakirja

--- shakki/main.py
+++ shakki/main.py
@@ -1,10 +1,27 @@
 from shakki import Shakki
 
-
 shakki = Shakki()
 
-shakki.pelilauta()
-soturit = shakki.nappulaRyhma("m", "S")
-ratsut = shakki.nappulaRyhma("v", "R")
-print(shakki.rLailliset("v", ratsut))
-print(shakki.dLailliset("v", shakki.nappulaRyhma("v", "D")))
+def peli():
+    shakki.pelilauta()
+    vuoro = "v"
+    while True:
+
+        siirrettava = input("syötä siirrettävän koordinaatti: ")
+        siirto = input("syötä siirron koordinaatti: ")
+
+        k1 = shakki.shakkiKoordinaatit[siirrettava] 
+        k2 = shakki.shakkiKoordinaatit[siirto]
+        
+        if not shakki.siirto(k1, k2):
+            if shakki.lauta[k1][0] != vuoro:
+                print("Siirto epäonnistui.")
+                continue
+
+        vuoro = shakki.vastVari(vuoro)
+        shakki.pelilauta()
+
+peli()
+
+
+        
